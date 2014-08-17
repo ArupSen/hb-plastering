8ca7cb165d9331635be4ac4cad6b229501412b5f
Arup Sen <arupsen48@gmail.com>
2014-08-17 12:50:55 +0100

    added nav items from sitemap

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,14 @@
 <?php
 // -- navbar.php --
 ?>
-<ul id="navbar">
+<ul id="navbar" class="nav nav-justified nav-pills">
 <li id="nav-home"><a href="index.php" title="home"><span class="tab" >home</span></a></li>
-<li id="nav-collections"><a href="collections.php" title="collections"><span class="tab">collections</span></a></li>
-<li id="nav-press"><a href="press.php" title="press"><span class="tab">press</span></a></li>
-<li id="nav-about"><a href="about.php" title="about"><span class="tab">about</span></a></li>
-<li id="nav-contact"><a href="contact.php" title="contact"><span class="tab">contact</span></a></li>
+<li id="nav-plastering"><a href="plastering.php" title="plastering"><span class="tab">plastering</span></a></li>
+<li id="nav-rendering"><a href="rendering.php" title="rendering"><span class="tab">rendering</span></a></li>
+<li id="nav-damp_proofing"><a href="damp_proofing.php" title="damp_proofing"><span class="tab">damp proofing</span></a></li>
+<li id="nav-gallery"><a href="gallery.php" title="gallery"><span class="tab">gallery</span></a></li>
+<li id="nav-qualification"><a href="qualification.php" title="qualification"><span class="tab">qualification</span></a></li>
+<li id="nav-quote"><a href="quote.php" title="quote"><span class="tab">quote</span></a></li>
 </ul>
 <div id="clear"></div>
 <div id="contents">
