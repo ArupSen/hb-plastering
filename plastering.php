1a5c4959685e70162ff664e8385df1ca12654460
Arup Sen <arupsen48@gmail.com>
2014-10-30 11:58:42 +0000

    added text from old site

--- plastering.php
+++ plastering.php
@@ -6,6 +6,12 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<h2>Plaster work in London and Kent</h2>
+<p>HB Plastering are proud to have built up a reputation for excellent quality and service.</p>
+<h3>Best for lasting plastering</h3>
+<p>HB Plastering create amazing wall finishes, giving you the flat surfacing required for painting and decorating. Enjoy premium walls, which are free from lumps or ridges.</p>
+<p>Take advantage of all the benefits of fast and effective plastering services. HB Plastering's specialists work quickly and efficiently, producing brilliant results at competitive rates. Pave the way for the very best in re-decoration with solid plaster.</p>
+<p>Request a quote for plaster work available throughout London and Kent.</p>
 
 
 <?php footer(); ?>
--- rendering.php
+++ rendering.php
@@ -6,6 +6,12 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<h2>External Rendering in London</h2>
+<p>Render your property a huge success! Enjoy improved insulation and save money on your utility bills with high quality external rendering carried out by the experts from Sydenham, London.
+<h3>For Solid Results</h3>
+<p>The quality of your exterior walling says a lot about you. HB Plastering use high-quality materials and time-served techniques to lay a range of rendering. For added charm, the render is textured, coloured or painted following application.</p>
+<p>Give your building the very best protection. Using layers coated with adhesive, HB Plastering quickly and efficiently insulate solid walls from the outside. Additional finishes include texturing and pebble dashing, giving your premises an entire image overhaul. Enjoy longer lasting-results with our domestic plasterers.</p>
+<p>Request a quote on external rendering offered by the experts from Sydenham, London.</p>
 
 
 <?php footer(); ?>
