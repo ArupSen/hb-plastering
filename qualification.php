7f9f7f3d125d72aea7540c68a057269b64a76e1d
Arup Sen <arupsen48@gmail.com>
2014-11-11 09:09:25 +0000

    added logo

--- qualification.php
+++ qualification.php
@@ -6,6 +6,10 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<h2>NVQ and City & Guilds qualified</h2>
+<?php
+img_tag('nvq_city_guilds_logo.jpg');
+?>
 
 
 <?php footer(); ?>
