c11bd49363d49d42ad8fb1cb28a8524f215fb855
Arup Sen <arupsen48@gmail.com>
2014-11-11 09:05:43 +0000

    added text

--- damp_proofing.php
+++ damp_proofing.php
@@ -6,6 +6,15 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
+<h2>Damp Proofing in Sydenham, London</h2>
+<p>Reinforce your walls with extra resilience. Protect your property from the harmful effects of damp with thorough and effective damp proofing available throughout Sydenham, London, and surrounding areas. Our domestic plasterers have the skills and expertise to get the job done.</p>
+<h3>Tough on Damp</h3>
+<p>Discover the easy method to stop moisture from harming your home. H B Plastering offer completely free no-obligation quotations, carrying out damp proof courses to insert physical barriers between your building and damp.</p>
+<p>There have been many different approaches to tackling damp over the years, with one of the earliest techniques being to use a layer of slate to prevent moisture from seeping through walls. This process has since been replaced with plastic alternatives, which are now more commonplace.</p>
+<h3>Preventative Solutions</h3>
+<p>Stop damage from condensation, and halt damp in its tracks. The most common cause of dampness is easily controlled by H B Plastering, who use a variety of damp coursing treatments. Our simple and straightforward process has been tried and tested over the years, giving you peace of mind.</p>
+<h3>Top for Tanking</h3>
+<p>Don't let anything dampen your property's potential. By fully waterproofing your cellar, you reduce the risk of water entering through walls. H B Plastering apply various waterproofing and tanking techniques suitable to the requirements of your property. Gain fast and effective results with us.</p>
+<p>Request a quote for damp proofing available throughout Sydenham, London and surrounding areas.</p>
 
 <?php footer(); ?>
