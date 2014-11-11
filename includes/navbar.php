3dbf866e2a52baad9d419647f58c97ab6b45ff99
Arup Sen <arupsen48@gmail.com>
2014-11-11 09:10:30 +0000

    changed services to testimonials

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,15 @@
 <?php
 // -- navbar.php --
 ?>
-<ul id="navbar" class="nav nav-justified nav-pills">
-<li id="nav-home"><a href="index.php" title="home"><span class="tab" >home</span></a></li>
-<li id="nav-plastering"><a href="plastering.php" title="plastering"><span class="tab">plastering</span></a></li>
-<li id="nav-rendering"><a href="rendering.php" title="rendering"><span class="tab">rendering</span></a></li>
-<li id="nav-damp_proofing"><a href="damp_proofing.php" title="damp_proofing"><span class="tab">damp proofing</span></a></li>
-<li id="nav-gallery"><a href="gallery.php" title="gallery"><span class="tab">gallery</span></a></li>
-<li id="nav-qualification"><a href="qualification.php" title="qualification"><span class="tab">qualification</span></a></li>
-<li id="nav-quote"><a href="quote.php" title="quote"><span class="tab">quote</span></a></li>
+<ul id="navbar" class="nav nav-justified nav-pills" role="navigation">
+  <li id="nav-home"><a href="index.php" title="home">home</a></li>
+  <li id="nav-plastering"><a href="plastering.php" title="plastering">plastering</a></li>
+  <li id="nav-rendering"><a href="rendering.php" title="rendering">rendering</a></li>
+  <li id="nav-damp_proofing"><a href="damp_proofing.php" title="damp proofing">damp proofing</a></li>
+  <li id="nav-gallery"><a href="gallery.php" title="gallery">gallery</a></li>
+  <li id="nav-qualification"><a href="qualification.php" title="qualification">qualification</a></li>
+  <li id="nav-quote"><a href="quote.php" title="quote">quote</a></li>
+  <li id="nav-testimonials"><a href="testimonials.php" title="testimonials">testimonials</a></li>
 </ul>
 <div id="clear"></div>
 <div id="contents">
