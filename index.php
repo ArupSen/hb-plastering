8124a165ac546c171ae49124ed2bf4e971dedbd6
Arup Sen <arupsen48@gmail.com>
2014-11-11 09:06:22 +0000

    updated text with client present

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ navbar();
 <h2>Plastering and Rendering Services in London and Kent</h2>
 <p>HB Plastering are proud to have built up an excellent reputation for quality work and services in London and Kent in the last 10 years. We offer a friendly service and a high quality standard of workmanship to all our customers.</p>
 <h3>HB Plastering are professional local tradesmen specialising in:</h3>
-<p>Plastering, rendering, damp proofing, Pebble dashing, Insulated Render, Tanking, Waterproofing and Crack Stitching.</p>
+<p>Plastering, Rendering, Lime Render, Insulated Render, Damp Proofing, Pebble Dashing, Tanking, Waterproofing, Crack Stitching and Painting and Decorating.</p>
 <h3>Why choose us?</h3>
-<p>Our promise to you is that we will arrive when we say we will and advise on the best way to complete our job to ensure a long lasting finish. We provide a quality, clean, tidy and professional service. Your property is our number one priority. Whether it's an area to be plastered, an entire property requiring rendering or pebble dashing or a basement needing damp proofing, we have it covered.</p>
-<p>HB Plastering pride ourselves on our fair pricing policy, we are fully insured and offer full guarantees on all of our work. Our team of qualified expert plasterers offer a whole host of packages for internal and external finishes throughout London and Kent.</p>
+<p>Our promise to you is that we will arrive when we say we will and advise on the best way to complete your job to ensure a long lasting finish. We provide a quality, clean, tidy and professional service. Your property is our number one priority. Whether it's an area to be plastered, an entire property requiring rendering or pebble dashing or a basement needing damp proofing, we have it covered.</p>
+<p>HB Plastering pride ourselves on our fair pricing policy, we are fully insured and qualified offering full guarantees on all of our work. Our team of qualified expert plasterers offer a whole host of packages for internal and external finishes throughout London and Kent.</p>
 <p>Take advantage of our wealth of expertise today and contact us with your specific requirements. We believe we will never be beaten on price or service so if you have had a quote from another plastering company please let us know and we will try to match it or beat it.</p>
 <p>Request a quote on any of our plastering services offered by skilled domestic plasterers based in South London.</p>
 
