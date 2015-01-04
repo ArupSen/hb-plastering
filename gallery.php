d0c338b874b6d9c42dbc68088c5a326c313d5a87
Arup Sen <arupsen48@gmail.com>
2015-01-04 00:47:49 +0000

    added captions to gallery and new images

--- gallery.php
+++ gallery.php
@@ -11,23 +11,64 @@ navbar();
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
 <?php
-for ($i = 1; $i < 27; $i++) {
-  if ($i == 1) {
+$slide_text = array(
+  "Hacking off existing pebble dash",
+  "Waterproof sand and cement scratch coat",
+  "Finished pebble dashing",
+  "Waterproof sand and cement render",
+  "Curved wall in waterproof sand and cement render",
+  "Waterproof sand and cement render",
+  "Waterproof sand and cement rendering",
+  "Waterproof rendered plinth",
+  "Restoring the original features",
+  "Restoration of original features",
+  "Restoring of the original features",
+  "Restoring original features",
+  "Flower bed",
+  "Flower bed",
+  "Smoothly finished render",
+  "Rendered flower bed",
+  "Plastered angled ceiling",
+  "Plastering a newly built kitchen",
+  "Plastering a new extension",
+  "Plastering lots of skylights and corners",
+  "Restoring original ceiling",
+  "Finish coat of insulated render",
+  "Insulated render",
+  "Insulated render",
+  "Insulated render",
+  "Restoring original Victorian ceiling",
+  "Keeping the original coving and ceiling rose",
+  "Front of the house finishing rendering",
+  "Sharp and neat render finish",
+  "Rendering",
+  "Pebble dashing with render finished corner",
+  "Rendering",
+  "Plastering ceiling with skylights",
+  "Side of house rendering",
+  "Entire property lime render",
+  "Entire property waterproof sand and cement render",
+  "Rendering",
+  "Rendering",
+  "Curved wall plastering",
+  "Original coving kept while ceiling replaced",
+  "Plastered ceiling",
+  "Waterproof sand and cement render",
+  "Waterproof sand and cement render",
+  "Entire property lime render",
+  "Lime render"
+);
+for ($i = 0; $i < 45; $i++) {
+  if ($i == 0) {
     echo '<div class="item active">';
   } else {
     echo '<div class="item">';
   }
   $next_slide = 'slide-'.$i.'.jpg';
   img_tag($next_slide);
-  if ($i == 9) {
     echo '<div class="carousel-caption">';
-    echo '<p>Sand and cement, waterproof render</p>';
+    echo "<p>$slide_text[$i]</p>";
     echo '</div>';
-  } else {
-    echo '<div class="carousel-caption">';
-    echo '<p>text describing the slide image goes here</p>';
-    echo '</div>';
-  }
   echo '</div>';
 }
 ?>
