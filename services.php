28c39036021134e910ee3e7b5b6be33ac304e14a
Arup Sen <arupsen48@gmail.com>
2015-01-04 00:55:53 +0000

    initial commit with full text

---
+++ services.php
@@ -0,0 +1,18 @@
+<?php
+// -- damp_proofing.php --
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<h2>Services</h2>
+<p>We are qualified professional local tradesmen, specialising in, Plastering, Rendering, Damp Proofing, Pebble Dashing, Insulated Render, Tanking and Water Proofing.</p>
+<p>H B Plastering offers friendly service &amp; a high quality standard of workmanship to all our customers. We are reliable, fully insured and also offer full guarantees on all of our work.</p>
+<h3>When looking for a plasterer why should you choose us?</h3>
+<p>Our promise to you is that we will arrive when we say we will, advise on the best way to complete your job to ensure a long lasting finish. We provide a professional quality, clean and tidy service. We pride ourselves in our fair pricing policy for all our customers.</p>
+<p>Our team of experienced and qualified plasterers can carry out all plastering related work and can offer a whole package of internal and external finishes. We are based in South East London. We cover all areas of London and Kent.</p>
+<p>Whether it's a small area to plaster, a basement needing damp proofing or an entire property that requires rendering or pebble dashing, we are happy to provide a free quote and friendly advice.</p>
+<p>We believe we will never be beaten on price or service! So if you have had a Quote from another plastering company please let us know and we will try to match or beat it.</p>
+
+<?php footer(); ?>
