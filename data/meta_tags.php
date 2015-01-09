26a2296e554fbf3b4673f28762814ea04b51346e
Arup Sen <arupsen48@gmail.com>
2015-01-09 18:02:59 +0000

    entered title and description for all pages

--- data/meta_tags.php
+++ data/meta_tags.php
@@ -1,34 +1,33 @@
 <?php
-// arrays of the meta tags for http://sarahnoor.com
+// arrays of the meta tags for hb plastering
 // // use the page file name as the array name
 // // -- meta_tags.php --
 //
 // // title - 80 characters
 // // description - 255 characters
-// // keywords - 1024 characters
 //
 $index = array(
-  'title' => '',
-  'description' => '',
-  'keywords' => '');
-  
-$about = array(
-  'title' => '',
-  'description' => '',
-  'keywords' => '');
-  
-$contact = array(
-  'title' => '',
-  'description' => '',
-  'keywords' => '');
-
-$collections = array(
-  'title' => '',
-  'description' => '',
-  'keywords' => '');
-
-$press = array(
-  'title' => '',
-  'description' => '',
-  'keywords' => '');
+  'title' => 'London and Kent local professional tradesmen',
+  'description' => 'Plastering, Rendering, Lime Render, Insulated Render, Damp Proofing, Pebble Dashing, Tanking, Waterproofing, Crack Stitching and Painting and Decorating.');
+$plastering = array(
+  'title' => 'plaster work available throughout London and Kent',
+  'description' => 'HB Plastering\'s specialists work quickly and efficiently, producing brilliant results at competitive rates. Pave the way for the very best in re-decoration with solid plaster.');
+$rendering = array(
+  'title' => 'external rendering | lime rendering | waterproof rendering',
+  'description' => 'Give your building the very best protection or hide external flaws with sand and cement water proof render, lime render, insulated render or pebble dash.');
+$damp_proofing = array(
+  'title' => 'effective damp proofing available throughout Sydenham, London, and surrounding areas',
+  'description' => 'Discover the easy method to stop moisture from harming your home. H B Plastering offer completely free no-obligation quotations, carrying out damp proof courses to insert physical barriers between your building and damp.');
+$gallery = array(
+  'title' => 'Photos showcasing our previous work | Plastering | Rendering | Damp proofing',
+  'description' => 'Waterproof sand and cement render, plastering tricky curves and corners.');
+$qualification = array(
+  'title' => 'HB Plastering is NVQ and City and Guilds Qualified',
+  'description' => 'HB Plastering based in Sydenham is NVQ and City and Guilds Qualified local professional tradesmen offering a friendly efficient service.');
+$quote = array(
+  'title' => 'a free no-obligation quote on any of our work',
+  'description' => 'You will receive a confirmation from us and a member of our team will call you to arrange to see your job and make a quote.');
+$testimonials = array(
+  'title' => 'Comments and Feedback from previous clients',
+  'description' => 'HB Plastering has provided an excellent service to many clients. See a list of customer feedback and reviews.');
 ?>
